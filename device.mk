cbabc5a2b5c085d11a47ad6ea4c975847cfe12e3
Nolen Johnson <johnsonnolen@gmail.com>
2023-07-15 00:02:57 -0400

    quill_tab: Only copy tablet_core_hardware
    * tablet_core_hardware declares all the rest.
    
    Change-Id: I29984bde02fd84107ae0a260660a4f48ded30edb

--- device.mk
+++ device.mk
@@ -23,8 +23,4 @@ DEVICE_PACKAGE_OVERLAYS += \
     device/nvidia/quill_tab/overlay
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.software.device_admin.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_admin.xml \
-    frameworks/native/data/etc/android.software.managed_users.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.managed_users.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
